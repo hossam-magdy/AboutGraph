fb9053136fef16854caa93f45de30756cc8aaa43
Hossam Magdy <hossam.magdy@outlook.com>
2018-06-29 15:33:42 +0200

    Add web-server-bundle for --dev

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
