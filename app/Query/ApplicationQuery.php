9a581f5877cfedba7103a6f2c5cc58179195bbfa
Hossam Magdy <hossam.magdy@outlook.com>
2018-06-30 00:14:19 +0200

    Switching to laravel

---
+++ app/Query/ApplicationQuery.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Query;
+
+use App\Application;
+use Folklore\GraphQL\Support\Query;
+use GraphQL;
+use GraphQL\Type\Definition\Type;
+
+class ApplicationQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'applications'
+    ];
+
+    public function type()
+    {
+        return Type::listOf(GraphQL::type('Application'));
+    }
+
+    public function args()
+    {
+        return [
+            'id' => ['name' => 'id', 'type' => Type::int()],
+            'name' => ['name' => 'name', 'type' => Type::string()]
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        if (isset($args['id'])) {
+            return Application::where('id', $args['id'])->get();
+        } else if (isset($args['name'])) {
+            return Application::where('name', $args['name'])->get();
+        } else {
+            return Application::all();
+        }
+    }
+}
---
+++ app/Type/ApplicationType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Type;
+
+use Folklore\GraphQL\Support\Type as GraphQLType;
+use GraphQL\Type\Definition\Type;
+
+/**
+ * @author Hossam Magdy <hossam.magdy@aboutyou.de>
+ */
+class ApplicationType extends GraphQLType
+{
+    protected $attributes = [
+        'name' => 'Application',
+        'description' => 'A user'
+    ];
+
+    /*
+    * Uncomment following line to make the type input object.
+    * http://graphql.org/learn/schema/#input-types
+    */
+    // protected $inputObject = true;
+
+    public function fields()
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'The id of the user'
+            ],
+            'name' => [
+                'type' => Type::string(),
+                'description' => 'The name of X'
+            ]
+        ];
+    }
+
+    // If you want to resolve the field yourself, you can declare a method
+    // with the following format resolve[FIELD_NAME]Field()
+    protected function resolveNameField($root, $args)
+    {
+        return strtolower($root->name);
+    }
+}
